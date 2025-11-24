9483dac4e063125fc441d699dd20fbd0faf2c12f
Abeni <abenezeryirgalem0@gmail.com>
2025-11-24 14:40:04 +0300

    eat(dart-basics): demonstrate arithmetic operations with examples and print outputs

---
+++ math.dart
@@ -0,0 +1,29 @@
+void main(){
+  
+// the + operator returns the sum of two numbers
+  var a = 10;
+  var b = 20;
+  var c = a + b;
+  print(c);
+  print("the sum of $a and $b is $c");
+
+// the - operator returns the difference of two numbers
+  var d = a - b;
+  print(d); 
+  print("the difference of $a and $b is $d");
+
+// the * operator returns the product of two numbers
+  var e = a * b;
+  print(e);
+  print("the product of $a and $b is $e");
+
+// the / operator returns the quotient of a division
+  var f = b / a;
+  print(f);
+  print("the quotient of $b and $a is $f");
+
+// the % operator returns the remainder of a division
+  var g = b % a;
+  print(g);
+  print("the remainder of $b and $a is $g");
+}
