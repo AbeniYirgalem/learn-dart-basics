f35b7bd7c6b46719ba46285dfbc53fa1f3acfaba
Abeni <abenezeryirgalem0@gmail.com>
2025-11-24 14:39:29 +0300

    feat: add variable examples using var, final, and const in main function

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  var name = "Abeni";
+  // name = 10; // we can not change the data type of a variable
+
+  print(name);
+
+  final age = 23;
+  // age = 30; // we can not change the value of a final variable
+  print(age);
+
+  const isOpen = true;
+  // isOpen = false; // we can not change the value of a const variable
+  print(isOpen);
+}
