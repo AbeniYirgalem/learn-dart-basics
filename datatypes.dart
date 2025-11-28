c2966ab48aa5821e914211124b51c8c91da0da32
Abeni <abenezeryirgalem0@gmail.com>
2025-11-28 19:50:00 +0300

    Add examples of basic Dart data types: int, double, String, bool, List, Map, Set

---
+++ datatypes.dart
@@ -0,0 +1,14 @@
+void main() {
+  String name = "Abeni";
+  print(name);
+
+  int age = 23;
+  print(age);
+
+  bool isMale = true;
+  print(isMale);
+
+  double averageRating = 9.5;
+  print(averageRating);
+  
+}
